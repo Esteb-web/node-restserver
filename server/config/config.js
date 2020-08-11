80ae05fcafbdea8cce7071b4e15142d0f79a43ad
Esteban <encuentroacademia@gmail.com>
2020-08-11 10:26:54 -0700

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://siervo_esteb:hgeehFp0em6eTXYH@cluster0.ga6mx.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
